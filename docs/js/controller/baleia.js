045f1a31451216d5c6b8e6ae8cb5a7966dcc09ac
Raoni Demarchi <raonidemarchi@gmail.com>
2017-11-18 04:27:24 -0200

    ok

---
+++ docs/js/controller/baleia.js
@@ -0,0 +1,3 @@
+$('#title').text('Baleia').attr('class', 'brand-logo white-text center');
+$('#hamburguer').attr('class', 'sidenav-trigger brand-logo white-text left');
+$('.nav-wrapper').attr('class', 'nav-wrapper grey darken-4');
--- docs/js/init.js
+++ docs/js/init.js
@@ -1,1 +1,28 @@
 let instance = new M.Sidenav(document.querySelector('.sidenav'));
+let $content = $('#content');
+
+FastClick.attach(document.body);
+
+$('a').click(function(e) {
+	e.preventDefault();
+	
+	instance.close();
+	
+	let href = $(this).attr('href');
+	
+	if(href == '#') return;
+	
+	$.ajax({
+		method: 'GET',
+		url: 'view/' + href + '.html',
+		dataType: 'text',
+		
+		success: function(data) {
+			$content.html(data);
+		}
+	});
+	
+	$.getScript('js/controller/' + href + '.js');
+});
+
+$('.sidenav > li:nth-child(2) > a').click();
